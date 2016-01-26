c2d2104b1d4b79802dbd551e6b58d8de3f5cd3c4
Joshua Leger <legermjoshua@gmail.com>
2016-01-26 11:38:22 -0700

    made all doubles

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -8,12 +8,12 @@ using namespace std;
 /**
  * This method determines the determinante of Martrix T
  */
-double getDeterminent(int x1, int x2, int x3, int y1, int y2, int y3)
+double getDeterminent(double x1, double x2, double x3, double y1, double y2, double y3)
 {
-    int a = x1 - x3;
-    int b = x2 - x3;
-    int c = y1 - y3;
-    int d = y2 - y3;
+    double a = x1 - x3;
+    double b = x2 - x3;
+    double c = y1 - y3;
+    double d = y2 - y3;
 
     return a*d - b*c;
 }
@@ -21,7 +21,7 @@ double getDeterminent(int x1, int x2, int x3, int y1, int y2, int y3)
 /**
  * This method calculates lambda 1
  */
-double getLambdaOne(int x, int y, int x1, int x2, int x3, int y1, int y2, int y3, double det)
+double getLambdaOne(double x, double y, double x1, double x2, double x3, double y1, double y2, double y3, double det)
 {
     return ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / det;
 }
@@ -29,7 +29,7 @@ double getLambdaOne(int x, int y, int x1, int x2, int x3, int y1, int y2, int y3
 /**
  * This method calculates lambda 2
  */
-double getLambdaTwo(int x, int y, int x1, int x2, int x3, int y1, int y2, int y3, double det)
+double getLambdaTwo(double x, double y, double x1, double x2, double x3, double y1, double y2, double y3, double det)
 {
     return ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / det;
 }
@@ -53,6 +53,7 @@ int main(int argc, char** argv) {
 
     for(int k = 0; k < 3; k++)
     {
+        cout << inputs[k] << endl;
         vector<string> strs, points, colors;
         boost::split(strs, inputs[k], boost::is_any_of(":"));
         boost::split(points, strs[0], boost::is_any_of(","));
@@ -66,32 +67,20 @@ int main(int argc, char** argv) {
         cout << "(" << values[k][0] << ", " << values[k][1] << "), R:" << values[k][2] << ", G:" << values[k][3] << ", B:" << values[k][4] << endl;
     }
 
-    /* 
-      Prompt user for 3 points separated by whitespace.
-
-      Part 1:
-          You'll need to get the x and y coordinate as floating point values
-          from the user for 3 points that make up a triangle.
-
-      Part 3:
-          You'll need to also request 3 colors from the user each having
-          3 floating point values (red, green and blue) that range from 0 to 1.
-    */
-
     // create an image 640 pixels wide by 480 pixels tall
     // with a standard 32 bit red, green, blue format
     QImage image(640, 480, QImage::Format_RGB32);
 
-    int x1 = values[0][0];
-    int x2 = values[1][0];
-    int x3 = values[2][0];
-    int y1 = values[0][1];
-    int y2 = values[1][1];
-    int y3 = values[2][1];
-    int minX = min(min(x1, x2), x3);
-    int maxX = max(max(x1, x2), x3);
-    int minY = min(min(y1, y2), y3);
-    int maxY = max(max(y1, y2), y3);
+    double x1 = values[0][0];
+    double x2 = values[1][0];
+    double x3 = values[2][0];
+    double y1 = values[0][1];
+    double y2 = values[1][1];
+    double y3 = values[2][1];
+    double minX = min(min(x1, x2), x3);
+    double maxX = max(max(x1, x2), x3);
+    double minY = min(min(y1, y2), y3);
+    double maxY = max(max(y1, y2), y3);
     double det = getDeterminent(x1, x2, x3, y1, y2, y3);
 
     double r1 = values[0][2];
@@ -120,38 +109,13 @@ int main(int argc, char** argv) {
                 double colorG = (g1 * lambda1 + g2 * lambda2 + g3 * lambda3) * 255;
                 double colorB = (b1 * lambda1 + b2 * lambda2 + b3 * lambda3) * 255;
 
-                if (lambda1 > 0 && lambda2 > 0 && lambda3 > 0)
+                if (lambda1 >= 0 && lambda2 >= 0 && lambda3 >= 0)
                 {
                     image.setPixel(i,j, qRgb(colorR,colorG,colorB));
                 }
             }
         }
     }
-
-    /* 
-      Part 1:
-          Calculate the bounding box of the 3 provided points and loop
-          over each pixel in that box and set it to white using:
-
-          image.setPixel(x,y, qRgb(255,255,255));
-
-      Part 2:
-          Modify your loop from part 1. Using barycentric coordinates, 
-          determine if the pixel lies within the triangle defined by 
-          the 3 provided points. If it is color it white, otherwise
-          move on to the next pixel.
-
-          For more on barycentric coordinates:
-          http://en.wikipedia.org/wiki/Barycentric_coordinate_system
-
-      Part 3:
-          For each pixel in the triangle, calculate the color based on
-          the calculated barycentric coordinates and the 3 provided
-          colors. Your colors should have been entered as floating point
-          numbers from 0 to 1. Since we have a 32 bit image, the red, 
-          green and blue components range from 0 to 255. Be sure to make
-          the conversion.
-    */
 
     if(image.save("triangle.jpg",0,100)) {
         cout << "Output triangle.jpg" << endl;
