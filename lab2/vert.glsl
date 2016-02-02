47ef4bcc0bdc3b43215eb11179a7b0e6647c046a
Joshua Leger <legermjoshua@gmail.com>
2016-02-02 10:03:34 -0700

    got working

--- lab2/vert.glsl
+++ lab2/vert.glsl
@@ -1,7 +1,10 @@
 #version 330
 
 in vec2 position;
+in vec3 color_in;
+out vec4 color;
 
 void main() {
   gl_Position = vec4(position.x, position.y, 0, 1);
+  color = vec4(color_in.r, color_in.g, color_in.b, 1.0);
 }
