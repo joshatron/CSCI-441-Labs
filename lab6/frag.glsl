57ed2910673bdd0907f87e996d4e6d56cb782e9b
Joshua Leger <legermjoshua@gmail.com>
2016-03-08 09:28:51 -0700

    a cube

--- lab6/frag.glsl
+++ lab6/frag.glsl
@@ -13,7 +13,7 @@ void main() {
     float diffuse = .5 * max(0, dot(fnormal, l));
     vec3 r = 2 * dot(fnormal, l) * fnormal - l;
     vec3 v = normalize(-1 * fposition);
-    float specular = .5 * power(max(0, dot(r, v)), 1);
+    float specular = .5 * pow(max(0, dot(r, v)), 1);
     float ambient = .1;
     color_out = vec4(fcolor * diffuse + vec3(specular) + fcolor * ambient, 1);
 }
